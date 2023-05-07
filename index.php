2172c9b92236f6d512130d4fa55b6585505be760
F-01-57suki <89096216+F-01-57suki@users.noreply.github.com>
2023-05-08 03:28:43 +0900

    メインページを新規作成

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>カウントダウン</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="wrapper">
+    <header>
+      <h1>カウントダウン</h1>
+    </header>
+    <main>
+      <div class="today">今日は何日</div>
+      <div class="bloc">
+        <h2>ここにタイトル</h2>
+        <p>残り何日</p>
+      </div>
+    </main>
+    <footer>
+      <p>copyright <?php echo date("Y"); ?>- Miyashita</p>
+    </footer>
+  </div>
+</body>
+</html>
